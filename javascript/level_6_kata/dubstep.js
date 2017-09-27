99808e188c6b04d0c88724f9b8c551f108ff231d
Mason Galland <masongalland@me.com>
2017-09-27 11:31:22 -0600

    write-out-numbers changes three digit number to string

--- javascript/level_6_kata/dubstep.js
+++ javascript/level_6_kata/dubstep.js
@@ -1,4 +1,7 @@
 /*
+
+https://www.codewars.com/kata/551dc350bf4e526099000ae5
+
 Polycarpus works as a DJ in the best Berland nightclub, and he often uses dubstep music in his performance. Recently, he has decided to take a couple of old songs and make dubstep remixes from them.
 
 Let's assume that a song consists of some number of words. To make the dubstep remix of this song, Polycarpus inserts a certain number of words "WUB" before the first word of the song (the number may be zero), after the last word (the number may be zero), and between words (at least one between any pair of neighbouring words), and then the boy glues together all the words, including "WUB", in one string and plays the song at the club.
--- javascript/level_6_kata/your_order_please.js
+++ javascript/level_6_kata/your_order_please.js
@@ -1,4 +1,7 @@
 /*
+
+https://www.codewars.com/kata/your-order-please/javascript
+
 Your task is to sort a given string. Each word in the String will contain a single number. This number is the position the word should have in the result.
 
 Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
@@ -26,3 +29,12 @@ function order(words){
   newArr.splice(0,1)
   return newArr.join(' ')
 }
+
+//////////////////////////////
+
+function order(words){
+  
+  return words.split(' ').sort(function(a, b){
+      return a.match(/\d/) - b.match(/\d/);
+   }).join(' ');
+}  
