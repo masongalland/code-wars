4f8d7f93e43aec3fe9e670ea816517e651bd7995
Mason Galland <masongalland@me.com>
2017-09-28 16:15:58 -0600

    finished sum_by_factors.js

--- javascript/level_4_kata/snail.js
+++ javascript/level_4_kata/snail.js
@@ -32,9 +32,3 @@ function snail(arr){
     return newArr;
 }
 
-var test = snail([  [1,2,3,4],
-                    [12,13,14,5],
-                    [11,16,15,6], 
-                    [10,9,8,7]])
-
-test 
