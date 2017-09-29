ea2d72dd442b59bdb26f463ce47c89500387a354
masongalland <masongalland@me.com>
2017-09-29 08:06:46 -0600

    finished automaton.js

--- javascript/level_4_kata/sum_by_factors.js
+++ javascript/level_4_kata/sum_by_factors.js
@@ -1,5 +1,13 @@
-// [ [p, sum of all ij of I for which p is a prime factor (p positive) of ij] ...]
-
+/*
+1. loop through main array (arr).
+2. In the loop, create an array (factors) of factors of each element in arr.
+3. loop through factors to test for prime values.
+4. if value is prime, check to see if it is in a nested array in newArr.
+5. if it is not in newArr, push an array to newArr (first value being the prime factor, second value being arr[i])
+6. if it is already in newArr, add arr[i] to the second value of the nested array
+7. return newArr
+ 
+*/
 
 function sumOfDivided(arr){
     let newArr = [];
@@ -30,21 +38,3 @@ function sumOfDivided(arr){
     }
     return newArr.sort((a,b) => a[0] - b[0])
  }
-
-/*
-1. loop through main array (arr).
-2. In the loop, create an array (factors) of factors of each element in arr.
-3. loop through factors to test for prime values.
-4. if value is prime, check to see if it is in a nested array in newArr.
-5. if it is not in newArr, push an array to newArr (first value being the prime factor, second value being arr[i])
-6. if it is already in newArr, add arr[i] to the second value of the nested array
-7. return newArr
-
-
-
-
-
-3. filter the factors array and keep prime values.
-4. Push a new object(with a key of original that has a value of arr[i] and a key called primes, which is the filtered array of prime factors) to an array (newArr) located in the root of the function.
-5. 
-*/
