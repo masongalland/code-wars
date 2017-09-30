052ca8d20244fafb75e39c09d2198bb339e2d1c0
Mason Galland <masongalland@me.com>
2017-09-30 15:57:18 -0600

    finished make_a_spiral.js

--- javascript/level_4_kata/snail.js
+++ javascript/level_4_kata/snail.js
@@ -1,34 +1,32 @@
-function snail(arr){
+function snail(arr) {
+  var benchmark = arr.length;
+  var counter = 0;
+  var newArr = [];
 
-    var benchmark = arr.length;
-    var counter = 0;
-    var newArr = [];
-
-    while(counter < 2){
-        for(var i = counter; i < benchmark; i++){
-            if(i === counter){
-                for(var l = counter; l < arr[i].length - counter; l++){
-                    newArr.push(arr[i][l])
-                }
-            }
-            if(i > counter){
-                newArr.push(arr[i][benchmark - 1])
-            }
+  while (counter < 2) {
+    for (var i = counter; i < benchmark; i++) {
+      if (i === counter) {
+        for (var l = counter; l < arr[i].length - counter; l++) {
+          newArr.push(arr[i][l]);
         }
-        for(var j = benchmark - 1; j > counter; j--){
-            if(j === benchmark - 1){
-                for(var k = arr[j].length - (2 + counter); k >= counter; k--){
-                    newArr.push(arr[j][k])
-                }
-            }
-            if(j < benchmark - 1){
-                newArr.push(arr[j][counter])
-            }
-        }
-        counter++;
-        benchmark--;
+      }
+      if (i > counter) {
+        newArr.push(arr[i][benchmark - 1]);
+      }
     }
+    for (var j = benchmark - 1; j > counter; j--) {
+      if (j === benchmark - 1) {
+        for (var k = arr[j].length - (2 + counter); k >= counter; k--) {
+          newArr.push(arr[j][k]);
+        }
+      }
+      if (j < benchmark - 1) {
+        newArr.push(arr[j][counter]);
+      }
+    }
+    counter++;
+    benchmark--;
+  }
 
-    return newArr;
+  return newArr;
 }
-
