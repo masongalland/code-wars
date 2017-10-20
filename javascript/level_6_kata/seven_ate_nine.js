7882ad05253ff4aca395641c4fc31ad867774c15
masongalland <masongalland@me.com>
2017-10-20 16:51:34 -0600

    finished seven_ate_nine.js

---
+++ javascript/level_6_kata/seven_ate_nine.js
@@ -0,0 +1,23 @@
+/*
+Seven is a hungry number and its favourite food is number 9. Whenever it spots 9 through the hoops of 8, it eats it! Well, not anymore, because you are going to help the 9 by locating that particular sequence (7,8,9) in an array of digits and tell 7 to come after 9 instead. Seven "ate" nine, no more! (If 9 is not in danger, just return the same array)
+
+
+Test.describe("Tests", function(){
+Test.assertSimilar(hungrySeven([7,8,9]), [8,9,7]);
+Test.assertSimilar(hungrySeven([7,7,7,8,9]),[8,9,7,7,7]);
+Test.assertSimilar(hungrySeven([8,7,8,9,8,9,7,8]),[8,8,9,8,9,7,7,8]);
+Test.assertSimilar(hungrySeven([8,7,8,7,9,8]),[8,7,8,7,9,8]);
+});
+
+*/
+
+const hungrySeven = a => {
+  let s = a.join("").toString();
+  if (!s.match(["789"])) {
+    return a;
+  } else {
+    s = s.replace(/789/gi, "897").split("");
+    s = s.map(c => c * 1);
+    return hungrySeven(s);
+  }
+};
