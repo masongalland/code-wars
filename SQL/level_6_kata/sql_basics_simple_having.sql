7dfeebcf877d780018e503d1a0be974366cd18e4
masongalland <masongalland@me.com>
2018-02-22 10:44:59 -0700

    finished sql basics simple having

---
+++ SQL/level_6_kata/sql_basics_simple_having.sql
@@ -0,0 +1,17 @@
+-- http://www.codewars.com/kata/sql-basics-simple-having/sql
+
+-- For this challenge you need to create a simple HAVING statement, you want to count how many people have the same age and return the groups with 10 or more people who have that age.
+
+-- people table schema
+-- id
+-- name
+-- age
+
+-- return table schema
+-- age
+-- total_people
+
+select age, count(*) as total_people
+from people
+group by age
+having count(*) >= 10
